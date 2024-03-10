fe8f29ba0f099e1b71997da5e877dbbe40bd6141
nabim777 <nabinalemagar019@gmail.com>
2024-03-10 11:11:03 +0545

    add headers components

---
+++ restro-project/src/components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="nav">
+        <a href="#">Home</a>
+        <a href="#">Add Resturant</a>
+        <a href="#">Update Resturant</a>
+        <a href="#">Logout</a>
+    </div>
+</template>
+
+<script>
+
+export default({
+    name: 'Header',
+})
+</script>
+
+<style>
+.nav{
+    background-color: #333;
+    overflow: hidden;
+
+}
+
+.nav a{
+    text-align: center;
+    color: #f2f2f2;
+    float: left;
+    padding: 16px 14px;
+    text-decoration: none;
+    font-size: 16px; 
+    margin-right: 4px;
+}
+
+.nav a:hover{
+    background-color: #ddd;
+    color:#222
+}
+
+</style>
--- restro-project/src/components/Home.vue
+++ restro-project/src/components/Home.vue
@@ -1,12 +1,17 @@
 <template>
+    <Header></Header>
     <h1>Hello, Welcome on home Page</h1>
 </template>
 
 <script>
 import router from '@/routes';
+import Header from './Header.vue'
 
 export default{
     name: "Home",
+    components: {
+        Header
+    },
     //mounted run whenever the page is reloaded
     mounted(){
         let user = localStorage.getItem("user-info")
