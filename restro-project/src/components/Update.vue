09e126418de8f20af094ff5bbbe7633f30fd78dd
nabim777 <nabinalemagar019@gmail.com>
2024-03-29 12:40:27 +0545

    update route link with id

--- restro-project/src/components/Update.vue
+++ restro-project/src/components/Update.vue
@@ -1,6 +1,12 @@
 <template>
     <Header></Header>
     <h1>Hello, Welcome on update resturant</h1>
+    <form class="add">
+        <input type="text" placeholder="Enter Name" v-model="resturant.name" name="name">
+        <input type="text" placeholder="Enter Address" v-model="resturant.address" name="Address">
+        <input type="text" placeholder="Enter Contact" v-model="resturant.contact" name="Contact">
+        <button type="button" v-on:click="updateResturant">Update new Resturant</button>
+    </form>
 </template>
 
 <script>
@@ -10,6 +16,18 @@ export default{
     name: "Home",
     components: {
         Header
+    },
+    data(){
+        return {
+            resturant : {
+                name: '',
+                address: '',
+                contact: ''
+            }
+        }
+    },
+    method:{
+
     },
     //mounted run whenever the page is reloaded
     mounted(){
