041b46833642b7e90e00db4944a05aa70860f176
Nalem7 <61624650+nabim777@users.noreply.github.com>
2024-04-11 20:38:20 +0545

    green green (#32)
    * green green
    
    * green green

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -65,7 +65,7 @@ export default{
 </script>
 
 <style>
-table, td, th{
+table, td, th {
     border-collapse: collapse;
     text-align: left;
     padding: 15px;
@@ -87,7 +87,7 @@ tr:nth-child(even) {
 }
 
 tr:hover {
-    background-color: skyblue;;
+    background-color: skyblue;
     color: white;
 }
 
@@ -99,8 +99,8 @@ td:first-child, th:first-child{
 button {
     margin-left: 20px; /* Adjust margin as needed */
     color: white;
-    background-color: rgb(216, 89, 89);;
-    border-color: rgb(216, 89, 89);;
+    background-color: rgb(216, 89, 89);
+    border-color: rgb(216, 89, 89);
 }
 
 button:hover {
